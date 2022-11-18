90fa1731e89fbfb505146d64e144b044da9e0b0c
GilmarMMJr <gilmar_mendonca@live.com>
2022-11-18 18:34:50 -0300

    Classes de model, manager, business, Provider para o User

---
+++ SwiftArquiteturas/API/Manager/UserManager.swift
@@ -0,0 +1,60 @@
+//
+//  UserManager.swift
+//  SwiftArquiteturas
+//
+//  Created by Gilmar Junior on 16/11/22.
+//
+
+import Foundation
+
+protocol UserManagerProtocol {
+    func login(email: String,
+               password: String,
+               successHandler: @escaping(UserModel) -> Void,
+               failureHandler: @escaping(Error?) -> Void)
+    func register(email: String,
+                  password: String,
+                  successHandler: @escaping(UserModel) -> Void,
+                  failureHandler: @escaping(Error?) -> Void)
+}
+
+class UserManager: UserManagerProtocol {
+    
+    let business: UserBusinessProtocol
+    
+    init(business: UserBusinessProtocol) {
+        self.business = business
+    }
+    
+    func login(email: String,
+               password: String,
+               successHandler: @escaping (UserModel) -> Void,
+               failureHandler: @escaping (Error?) -> Void) {
+        business.login(email: email, password: password) { result in
+            switch result {
+                
+            case .success(let userModel):
+                successHandler(userModel)
+            case .failure(let error):
+                failureHandler(error)
+            }
+        }
+    }
+    
+    func register(email: String,
+                  password: String,
+                  successHandler: @escaping (UserModel) -> Void,
+                  failureHandler: @escaping (Error?) -> Void) {
+        business.register(email: email, password: password) { result in
+            switch result {
+                
+            case .success(let userModel):
+                successHandler(userModel)
+            case .failure(let error):
+                failureHandler(error)
+            }
+        }
+    }
+    
+    
+}
---
+++ SwiftArquiteturas/Infra/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SwiftArquiteturas
+//
+//  Created by Gilmar Junior on 15/11/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct ParametersKeys {
+        static let body = "body"
+        static let userModel = "userModel"
+    }
+}
